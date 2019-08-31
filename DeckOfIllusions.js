6179e2829d815bb5a36658a57c14351aa1cab969
Ben L <39037350+blawson69@users.noreply.github.com>
2019-08-30 19:19:48 -0700

    Update DeckOfIllusions.js
    Corrected Venmo link in README

--- DeckOfIllusions.js
+++ DeckOfIllusions.js
@@ -4,7 +4,7 @@ Automates the Deck of Illusions wondrous item for D&D 5e games on Roll20
 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Like this script? Buy me a coffee: https://venmo.com/theBenLawson
+Like this script? Buy me a coffee: https://venmo.com/theRealBenLawson
 */
 
 var DeckOfIllusions = DeckOfIllusions || (function () {
